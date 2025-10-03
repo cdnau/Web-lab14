445498dfec5f9a1dc70ce392c114525d5c9ccd3f
unknown <you@example.com>
2025-10-03 10:07:03 +0700

    3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const myinput = document.getElementById("myinput")
+const my_input = document.getElementById("my_input")
+let current_value = 1
 
-function add() {
-    myinput.value = Number(myinput.value())
+function calc(x) {
+	current_value += Number(x) // Calculate Variable `current_value`
+	my_input.value = current_value // Set my_input Value to `current_value`
 }
